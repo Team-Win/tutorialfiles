6926cbff31f8c3c72f7ae99fec44f9c8c9408c99
Cam Hovell <camhovell@gmail.com>
2015-03-10 12:07:27 +1300

    Initial commit of more ruby crap

---
+++ moreruby.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  get '/' => 'pages#home'
+
+  resources :categories
+  get 'categories/:id/delete' => 'categories#delete', :as => :categories_delete
+
+  resources :products
+  get 'products/:id/delete' => 'products#delete', :as => :products_delete
+end
